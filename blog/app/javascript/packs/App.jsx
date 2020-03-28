ce4b211aaa6cf3c6f8ed59fb05a4d2064e911318
sidkoparde <sid@jewlr.com>
2020-03-27 22:42:55 -0400

    Finish tutorial

--- blog/app/javascript/packs/App.jsx
+++ blog/app/javascript/packs/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Footer from './components/Footer'
-import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
+import AddTodo from './containers/AddTodo'
+import VisibleTodoList from './containers/VisibleTodoList'
 
-export default App = () => (
+const App = () => (
     <div>
-        Hey
         <AddTodo />
         <VisibleTodoList />
         <Footer />
     </div>
 )
+
+export default App
--- blog/app/javascript/packs/components/Footer.jsx
+++ blog/app/javascript/packs/components/Footer.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import FilterLink from '../containers/FilterLink'
 import { VisibilityFilters } from '../actions'
 
-const Footer = () => {
+const Footer = () => (
     <div>
         <span>Show: </span>
         <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
         <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
         <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
     </div>
-}
+)
 
 export default Footer
